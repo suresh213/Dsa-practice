f99eab8e9419bd4f1e045ba6f7cc983a1cfc477d
suresh213 <asuresh52012@gmail.com>
2021-08-02 23:02:49 +0530

    added array problems

---
+++ Array/9.Pascal's Triangle.cpp
@@ -0,0 +1,14 @@
+//Time complexity :o(n)
+//space complexity:o(n)
+
+vector<vector<int>> generate(int n) {
+    vector<vector<int>> ans;
+    for(int i=0;i<n;i++){
+        vector<int> temp(i+1,1);
+        for(int j=1;j<i;j++){
+            temp[j] = ans[i-1][j-1]+ans[i-1][j];
+        }
+        ans.push_back(temp);
+    }
+    return ans;
+}
